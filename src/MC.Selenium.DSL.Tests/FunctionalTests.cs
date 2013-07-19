02e7a300e3ce3708d4e1304180bd2590a0f1db0e
matt-cochran <matthew@cochranweb.com>
2013-07-18 22:27:42 -0400

    fix tests

--- src/MC.Selenium.DSL.Tests/FunctionalTests.cs
+++ src/MC.Selenium.DSL.Tests/FunctionalTests.cs
@@ -81,8 +81,8 @@ namespace MC.Selenium.DSL.Tests
 
             driver.ExecuteCommand("go to " + url);
             driver.ExecuteCommand("assert text area with id 'x' has text with value 'text'");
-            //driver.ExecuteCommand("clear text area with id 'x'");
-            //driver.ExecuteCommand("assert textarea with id 'x' has text with value ''");
+            driver.ExecuteCommand("clear text area with id 'x'");
+            driver.ExecuteCommand("assert textarea with id 'x' has text with value ''");
 
             var val = Guid.NewGuid().ToString();
 
